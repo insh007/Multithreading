4dfe2d43157344ff6474f0937eedae12e4f5c263
insh007 <aliinshad007@gmail.com>
2024-11-17 14:56:59 +0530

    setPriority method of Thread class

--- ThreadClassMethods/src/MyClass.java
+++ ThreadClassMethods/src/MyClass.java
@@ -1,21 +1,28 @@
 public class MyClass extends Thread{
+
+    // To give custom name to Thread
+    public MyClass(String name){
+        super(name);
+    }
+
     @Override
     public void run() {
-        System.out.println("t1 before sleep"); // This will print first
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for(int i=1; i<=5; i++){
+            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " - Count: " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        System.out.println("t1 thread"); // This will print 2nd when you are using join method
+
     }
 
     public static void main(String[] args) throws InterruptedException {
-        MyClass t1 = new MyClass();
+        MyClass t1 = new MyClass("bond");
+        t1.setPriority(Thread.MIN_PRIORITY);
         t1.start();
 
-        t1.join(); // this method completely execute current thread(t1 Thread here)
 
-        System.out.println("Hello world!"); // This will print 3rd when you are using join method
     }
 }
